a7bfd64a87ad76ac968bcdb34c58c5f7593c2dcf
Ste11front <137266358+Ste11front@users.noreply.github.com>
2023-08-03 13:15:48 +0200

    task

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+// es. creare una pagina web che permetta di visualizzare gli ultimi 25 post del subreddit /aww (https://www.reddit.com/r/aww/new.json)
+
+// Reddit viewer:
+// - all'avvio (se non ho mai visitato il sito) presenterà la dialog di selezione dei subreddit dove potrò scegliere tra una decina di subreddit (obbligatorio almeno uno)
+// - la pagina principale mostrerà toolbar con un bottone per ogni subreddit selezionato che permetterà di scegliere quali post vedere
+// - tasto + che permetta di riaprire la dialog e aggiungere/cambiare i subreddit
+// - farlo bello!!! (estetica)
+
+// Funzionalità aggiuntive:
+// - aggiungere alla dialog la possibilità di aggiungere subreddit non previsti nella lista tramite un input di tipo testo
+// - aggiungere la possibilità di cambiare tra (new, hot, pop)
+
+// Super task:
+// - gestire la paginazione
+// - trasformare l'app in un gestore di rss
+
+
 function changeTitleColor(){
     const header = document.getElementById('header');
     // header.setAttribute('title-color', 'pink')
@@ -8,42 +25,26 @@ function changeTitleColor(){
     }
 }
 
-// es
-// fetch('https://www.reddit.com/r/aww/new.json')
-// .then(resp => resp.json())
-// .then(res => {
-//     const data = res.data;
-//     const posts = data.children;
-//     console.log(posts)
-// })
+// function toHumanTime(timestamp) {    // funzione per convertire orario
 
-// creare una pagina web che permetta di visualizzare
-// gli ultimi 25 post del subreddit /aww (https://www.reddit.com/r/aww/new.json)
-// usare i web component
+//     const timestampInMills = timestamp * 1000;
+//     const now = Date.now();
+//     const delta = now - timestampInMills;
 
-// 1) permettere all'utente di cambiare il tema
+//     const seconds = parseInt(delta / 1000);
+//     if (seconds < 60) {
+//         return seconds + ' secondi fa...';
+//     }
 
+//     const minutes = parseInt(seconds / 60);
+//     if (minutes < 60) {
+//         return minutes + ' minuti fa...';
+//     }
 
-function toHumanTime(timestamp) {    // funzione per convertire orario
-
-    const timestampInMills = timestamp * 1000;
-    const now = Date.now();
-    const delta = now - timestampInMills;
-
-    const seconds = parseInt(delta / 1000);
-    if (seconds < 60) {
-        return seconds + ' secondi fa...';
-    }
-
-    const minutes = parseInt(seconds / 60);
-    if (minutes < 60) {
-        return minutes + ' minuti fa...';
-    }
-
-    const hours = parseInt(minutes / 60);
-    if (hours < 24) {
-        return hours + ' ore fa...';
-    }
+//     const hours = parseInt(minutes / 60);
+//     if (hours < 24) {
+//         return hours + ' ore fa...';
+//     }
     
-    return parseInt(hours / 24) + ' giorni fa...';
-}
+//     return parseInt(hours / 24) + ' giorni fa...';
+// }
